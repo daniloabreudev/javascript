88d6075aaa081b860f1794a8ae0bc558e759c393
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-07-16 15:22:35 -0300

    adiciona exercicio

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.6
+if(vel <= 60.5){
+    console.log(`${vel}KM/hr: Velocidade Permitida. Boa Viagem!`)
+}else{
+    console.log(`${vel}KM/hr: Sua velocidade está acima da média! MULTADO`)
+}
+
