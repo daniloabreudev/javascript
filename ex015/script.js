9d43d1b3b76ec01553ff5d77d4584b38d3f5db05
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-07-28 14:05:41 -0300

    adiciona novas categorias de idade

--- ex015/script.js
+++ ex015/script.js
@@ -20,12 +20,35 @@ function verificar() {
 
         if (listaSexo[0].checked) {
             genero = 'Mulher'
-            img.src = 'foto-mulher.png'
-            img.alt = 'foto de uma mulher'
+            if (idade <12){
+                img.src = 'menina-criança.jpg'
+                img.alt = 'foto de uma menina'
+            }else if (idade < 18){
+                img.src = 'menina-jovem.jpg'
+                img.alt = 'foto de uma jovem'
+            }else if (idade < 60){
+                img.src = 'menina-adulta.jpg'
+                img.alt = 'foto de uma mulher adulta'
+            }else{
+                img.src = 'menina-idosa.jpg'
+                img.alt = 'foto de uma mulher idosa'
+            }
+            
         } else if (listaSexo[1].checked) {
             genero = 'Homem'
-            img.src = 'foto-homem.png'
-            img.alt = 'foto de um homem'
+            if (idade <12){
+                img.src = 'menino-crianca.jpg'
+                img.alt = 'foto de uma menino'
+            }else if (idade < 18){
+                img.src = 'menino-jovem.jpg'
+                img.alt = 'foto de um jovem'
+            }else if (idade < 60){
+                img.src = 'menino-adulto.jpg'
+                img.alt = 'foto de um homem adulto'
+            }else{
+                img.src = 'menino-idoso.jpg'
+                img.alt = 'foto de um homem idoso'
+            }
         }
 
         var nomeDigitado = nomeInput.value.toUpperCase()
