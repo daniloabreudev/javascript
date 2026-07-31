b4a47ebce5ce45dda044094ed5dc44c01244802e
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-07-31 16:06:22 -0300

    cria o exercicio da tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert("Olá")
+function gerarTabuada(){
+    let num = window.document.getElementById('txtnum')
+    let res = window.document.getElementById('res')
+
+    if(num.value.length == 0){
+        window.alert("Tente novamente. Digite um número válido")
+    }else{
+        let cont = 1
+        let numero_formatado = Number(num.value)
+
+        res.innerHTML = ''
+        while(cont <= 10){
+            res.innerHTML += `${cont} X ${numero_formatado} = ${cont * numero_formatado}<br>`
+            cont += 1
+        }
+    }
+}
