8354a49db8166939449b77eb42229c45b71f49c3
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-07-31 14:51:06 -0300

    add o desafio da contagem com js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,38 +1,32 @@
 function contar() {
-    var ini = document.getElementById('inicio')
-    var fim = document.getElementById("fim")
-    var passo = document.getElementById("passo")
-    var res = document.getElementById("res")
+    let ini = window.document.getElementById('txti') 
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let res = window.document.getElementById('res')
 
-    // 1. Corrigido .length em todos os campos
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossível contar! Preencha todos os dados.'
-        window.alert('[ERRO] Faltam dados!')
+        window.alert('[ERRO] Faltam dados! ')
     } else {
         res.innerHTML = 'Contando: <br>'
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-
-        // 2. Garante que p seja pelo menos 1
-        if (p <= 0) {
-            window.alert("Passo inválido! Considerando PASSO 1")
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        
+        if(p<=0){
+            window.alert('Passo inválido! Considere como PASSO 1')
             p = 1
         }
-
-        // 3. Estrutura de contagem
-        if (i < f) {
-            // Contagem crescente
-            for (var c = i; c <= f; c += p) {
+        if(i < f){
+            for (let c = i;  c <= f; c+=p){
+                res.innerHTML += `${c}\u{1F449}`
+            }
+        }else {
+            for (let c = i;  c >= f; c-=p){
                 res.innerHTML += `${c} \u{1F449} `
             }
-        } else {
-            // Contagem decrescente
-            for (var c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{1F449} `
-            }
-        } // Chave do else fechada corretamente aqui!
+        
+        }
 
-        res.innerHTML += `\u{1F3C1}` // Bandeirinha de chegada no final
+        res.innerHTML += `FIM\u{1F3C1}`
     }
 }
