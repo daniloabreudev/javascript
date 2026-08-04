330886cd5dd570d251838b06df0b637c6a45a8c8
Danilo Abreu <abreudanilojdas456@gmail.com>
2026-08-04 14:36:51 -0300

    estudos de funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,17 @@
+// function parouimpar(num){
+//     if(num % 2 == 0){
+//         return 'par'
+//     }
+//     return 'ìmpar'
+// }
+//  let num = 763
+// let res = parouimpar(num)
+// console.log(`O número ${num} é ${res} `)
+
+
+function soma(n1 = 0,n2 = 0){
+    return n1 + n2
+}
+n1 = 1
+n2 = 142
+console.log(`A soma entre ${n1} e ${n2} é ${soma(n1,n2)}`)
